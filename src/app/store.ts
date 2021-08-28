3f3c13a6a17ae9de3ecd6e9ececeaba7b5f710b9
Hiroki Ito <diavolos1230@gmail.com>
2021-08-28 20:19:40 +0900

    Coronavirus COVID19 API使用

---
+++ src/app/store.ts
@@ -0,0 +1,19 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+import covideReducer from '../features/covid/covidSlice';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    covid: covideReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
